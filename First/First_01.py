8b905f28693db3b133d10f7e584593d31de2256b
coyotezxy <coyotezxy@163.com>
2019-12-19 10:51:57 +0800

    First Commit

---
+++ First/First_01.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#@Time    : 2019-12-19 10:50
+#@Author  : SHAUN-coyote
+#@Email   : coyotezxy@163.com
+#@File    : First_01.py
---
+++ First/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#@Time    : 2019-12-19 10:49
+#@Author  : SHAUN-coyote
+#@Email   : coyotezxy@163.com
+#@File    : __init__.py.py
